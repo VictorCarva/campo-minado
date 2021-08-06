acaa8778856eb5f80be6964e664658280f3953c3
VictorCarva <86257351+VictorCarva@users.noreply.github.com>
2021-08-06 19:58:48 -0300

    Update Program.cs

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Ivory.TesteEstagio.CampoMinado
 {
@@ -11,8 +11,53 @@ namespace Ivory.TesteEstagio.CampoMinado
             Console.WriteLine(campoMinado.Tabuleiro);
 
             // Realize sua codificação a partir deste ponto, boa sorte!
-           
+
+            // Parametros para marcação do tabuleiro.
+            int recebeColuna, recebeLinha;
+
+            do
+            {
+                Console.WriteLine("\nDigite o Valor da posição ");
+
+                Console.Write("Linha: ");
+                recebeLinha = int.Parse(Console.ReadLine());
+
+                Console.Write("\nColuna: ");
+                recebeColuna = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                campoMinado.Abrir(recebeLinha, recebeColuna);
+
+                //Condicional para informar o andamento do jogo
+                if (campoMinado.JogoStatus == 0)
+                {
+                    Console.WriteLine("Jogo em Andamento\n=========");
+                }
+                else
+                {
+                    Console.WriteLine("Jogo Finalizado\n=========");
+                }
+
+                Console.WriteLine(campoMinado.Tabuleiro);
+
+            } 
+            while (campoMinado.JogoStatus == 0);
+
+            if (campoMinado.JogoStatus == 2)
+            {
+                Console.WriteLine("\nGameOver");
+            }
+            else
+            {
+                Console.WriteLine("\nVitoria");
+            }
 
         }
+
+
+
+
     }
 }
+
